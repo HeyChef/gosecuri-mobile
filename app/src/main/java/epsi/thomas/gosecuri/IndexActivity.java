56ef92bf11e9ad37a483f76e48e4d066965288f1
HeyChef <thomas59390@numericable.fr>
2019-05-10 17:12:38 +0200

    Firebase commit

--- app/src/main/java/epsi/thomas/gosecuri/IndexActivity.java
+++ app/src/main/java/epsi/thomas/gosecuri/IndexActivity.java
@@ -1,73 +1,199 @@
 package epsi.thomas.gosecuri;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
 
-import epsi.thomas.gosecuri.api.ocr.OcrCni;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabel;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabeler;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+import com.google.firebase.ml.vision.text.RecognizedLanguage;
+
+import java.util.List;
 
 public class IndexActivity extends AppCompatActivity {
 
     private ImageView imageView;
     private Button btnCamera;
     private Button btnValidate;
-    private File tempFile;
-    private final String TEMP_FILE_NAME = "temp_img_file.png";
+    private Uri imageUri;
+    private Bitmap thumbnail;
+    private Bitmap myBitmap;
+    private Button btnRotate;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
+    private static final int CAMERA_REQUEST = 1888;
+    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 200;
+    private static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 300;
 
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
 
+        FirebaseApp.initializeApp(this);
+
+
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+            return;
+        }
+
+        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+            return;
+        }
+
         btnCamera = findViewById(R.id.btnCamera);
         btnValidate = findViewById(R.id.btnValidate);
         imageView = findViewById(R.id.imageView);
-
-        File cDir = getBaseContext().getCacheDir();
-        tempFile = new File(cDir.getPath() + "/" + TEMP_FILE_NAME) ;
-
+        btnRotate = findViewById(R.id.btnRotate);
 
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent,0);
+                if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
+                } else {
+                    ContentValues values = new ContentValues();
+                    values.put(MediaStore.Images.Media.TITLE, "IDCard");
+                    imageUri = getContentResolver().insert(
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+                    Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                    startActivityForResult(intent, CAMERA_REQUEST);
+                }
             }
         });
 
         btnValidate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new OcrCni(IndexActivity.this, getBaseContext().getCacheDir().getPath() + "/" + TEMP_FILE_NAME).execute();
+                FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(((BitmapDrawable)imageView.getDrawable()).getBitmap());
+                //testFirebase(image);
+                testLabeler(image);
+            }
+        });
+
+        btnRotate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
             }
         });
     }
 
     @Override
-    protected void onActivityResult (int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode, resultCode, data);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            try {
+                thumbnail = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                //thumbnail = rotateBitmap(thumbnail,270);
 
-        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                imageView.setImageBitmap(thumbnail);
+                btnValidate.setVisibility(View.VISIBLE);
+                btnRotate.setVisibility(View.VISIBLE);
 
-        imageView.setImageBitmap(bitmap);
-        btnValidate.setVisibility(View.VISIBLE);
-        OutputStream outStream = null;
-        try {
-            outStream = new FileOutputStream(tempFile);
-            bitmap.compress(Bitmap.CompressFormat.PNG, 85, outStream);
-            outStream.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+                FirebaseVisionImage image = FirebaseVisionImage.fromFilePath(getApplicationContext(), imageUri);
+                testFirebase(image);
+                testLabeler(image);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    private void testFirebase(FirebaseVisionImage image) {
+        FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance()
+                .getOnDeviceTextRecognizer();
+        Task<FirebaseVisionText> task = detector.processImage(image);
+        task.addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+            @Override
+            public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                firebaseSuccess(firebaseVisionText);
+            }
+        });
+    }
+
+    private void testLabeler(FirebaseVisionImage image) {
+        FirebaseVisionImageLabeler labeler = FirebaseVision.getInstance()
+                .getCloudImageLabeler();
+        Task<List<FirebaseVisionImageLabel>> task = labeler.processImage(image);
+        task.addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionImageLabel>>() {
+            @Override
+            public void onSuccess(List<FirebaseVisionImageLabel> labels) {
+                for (FirebaseVisionImageLabel label: labels) {
+                    String text = label.getText();
+                    String entityId = label.getEntityId();
+                    float confidence = label.getConfidence();
+                    Log.i("Labeltest", text);
+                    Log.i("Labeltest", entityId);
+                }
+            }
+        });
+    }
+
+    private void firebaseSuccess(FirebaseVisionText result) {
+        String resultText = result.getText();
+        for (FirebaseVisionText.TextBlock block : result.getTextBlocks()) {
+            String blockText = block.getText();
+            Float blockConfidence = block.getConfidence();
+            List<RecognizedLanguage> blockLanguages = block.getRecognizedLanguages();
+            Point[] blockCornerPoints = block.getCornerPoints();
+            Rect blockFrame = block.getBoundingBox();
+            for (FirebaseVisionText.Line line : block.getLines()) {
+                String lineText = line.getText();
+                Float lineConfidence = line.getConfidence();
+                List<RecognizedLanguage> lineLanguages = line.getRecognizedLanguages();
+                Point[] lineCornerPoints = line.getCornerPoints();
+                Rect lineFrame = line.getBoundingBox();
+                for (FirebaseVisionText.Element element : line.getElements()) {
+                    String elementText = element.getText();
+                    Float elementConfidence = element.getConfidence();
+                    List<RecognizedLanguage> elementLanguages = element.getRecognizedLanguages();
+                    Point[] elementCornerPoints = element.getCornerPoints();
+                    Rect elementFrame = element.getBoundingBox();
+                    Log.i("firebaseSuccess", elementText);
+                }
+            }
+        }
+    }
+
+    public static Bitmap rotateBitmap(Bitmap source, float angle)
+    {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
